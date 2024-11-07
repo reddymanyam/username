458f5dc1c92bc51a15859501b18b2b88d896aea8
reddymanyam <reddappareddy18@gmail.com>
2024-11-07 10:14:07 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,18 @@ import Counter from './Counter';
 
 const App = () => {
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
   return (
     <div>
 
       <div style={{display:'flex', padding:"1px"}}>
-        <Navbar name={name} email={email} />
+        <Navbar />
         <Counter />
       </div>
 
       <div style={{ display: "flex", height: "91vh", justifyContent: "space-between" }}>
-        <Sidebar name={name} email={email} />
-        <Feed name={name} setName={setName} email={email} setEmail={setEmail} />
-        <Rightbar name={name} email={email} />
+        <Sidebar />
+        <Feed />
+        <Rightbar />
       </div>
 
     </div>
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../slice/counterSlice";
+import userReducer from "../slice/userSlice";
 
 const store = configureStore({
     
     reducer:{
         counter: counterReducer,
+        userupdate: userReducer,
     },
 });
 
